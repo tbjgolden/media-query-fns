23018339aa917ab66568578bd51c31f4465de108
Tom <8162045+tbjgolden@users.noreply.github.com>
2022-03-19 14:51:47 +0000

    feat: matches pt 2, found bugs

--- src/matches.test.ts
+++ src/matches.test.ts
@@ -1,21 +1,38 @@
 import { compileQuery } from "./compile";
-import { matches } from "./matches";
+import { Environment, matches } from "./matches";
+import util from "util";
 
-test("matches", () => {
-  const query = compileQuery("@media (max-width: 1400px)");
-  const DEFAULT_DIMENSIONS = {
-    widthPx: 1280,
-    heightPx: 800,
-    dppx: 2,
-    deviceWidthPx: 1280,
-    deviceHeightPx: 800,
+const DEFAULT_DIMENSIONS = {
+  widthPx: 1280,
+  heightPx: 800,
+  dppx: 2,
+  deviceWidthPx: 1280,
+  deviceHeightPx: 800,
+};
+
+test("matches width", () => {
+  const check = (query: string, diffs: Partial<Environment> = {}): boolean => {
+    const compiled = compileQuery(query);
+    console.log(
+      util.inspect(compiled, {
+        depth: 10,
+        colors: true,
+      })
+    );
+    return matches(compiled, {
+      ...DEFAULT_DIMENSIONS,
+      ...diffs,
+    });
   };
 
-  expect(matches(query, DEFAULT_DIMENSIONS)).toBe(true);
-  expect(
-    matches(query, {
-      ...DEFAULT_DIMENSIONS,
-      widthPx: 1600,
-    })
-  ).toBe(false);
+  expect(check("@media (max-width: 1400px)")).toBe(true);
+  expect(check("@media (min-width: 1000px)")).toBe(true);
+  expect(check("@media (min-width: 1400px)")).toBe(false);
+  expect(check("@media (width: 1280px)")).toBe(true);
+  expect(check("@media (width = 1280px)")).toBe(true);
+  expect(check("@media (1280px = width)")).toBe(true);
+  expect(check("@media (1280px <= width <= 1280px)")).toBe(true);
+  expect(check("@media (1280px < width <= 1280px)")).toBe(false);
+  expect(check("@media (1280px <= width < 1280px)")).toBe(false);
+  expect(check("@media (1280px < width < 1280px)")).toBe(false);
 });
--- src/matches.ts
+++ src/matches.ts
@@ -19,6 +19,7 @@ export type Environment = {
   deviceWidthPx: Integer;
   deviceHeightPx: Integer;
   colorBits: Integer;
+  monochromeBits: "not-monochrome" | Integer;
   colorTable: "none" | Integer;
   dppx: Integer;
 };
@@ -36,7 +37,8 @@ type DefaultableFeatures =
   | "scan"
   | "update"
   | "colorTable"
-  | "colorBits";
+  | "colorBits"
+  | "monochromeBits";
 
 export const DESKTOP_ENVIRONMENT: Pick<Environment, DefaultableFeatures> = {
   mediaType: "screen",
@@ -52,6 +54,7 @@ export const DESKTOP_ENVIRONMENT: Pick<Environment, DefaultableFeatures> = {
   update: "fast",
   colorTable: "none",
   colorBits: 8,
+  monochromeBits: "not-monochrome",
 };
 
 const badInput = (key: string): Error => {
@@ -132,6 +135,12 @@ export const validateEnv = (env: Environment) => {
     throw badInput("dppx");
   }
   if (
+    env.monochromeBits !== "not-monochrome" &&
+    !(Number.isInteger(env.monochromeBits) && env.monochromeBits >= 0)
+  ) {
+    throw badInput("monochromeBits");
+  }
+  if (
     env.colorTable !== "none" &&
     !(Number.isInteger(env.colorTable) && env.colorTable >= 0)
   ) {
@@ -184,13 +193,225 @@ export const matches = (
             break;
           }
         }
+      } else if (k === "any-hover") {
+        const v = p[k];
+        if (v !== env.anyHover) {
+          matches = false;
+          break;
+        }
+      } else if (k === "hover") {
+        const v = p[k];
+        if (v !== env.hover) {
+          matches = false;
+          break;
+        }
+      } else if (k === "any-pointer") {
+        const v = p[k];
+        if (v !== env.anyPointer) {
+          matches = false;
+          break;
+        }
+      } else if (k === "pointer") {
+        const v = p[k];
+        if (v !== env.pointer) {
+          matches = false;
+          break;
+        }
+      } else if (k === "grid") {
+        const v = p[k];
+        if (
+          (v === 0 && env.grid === "grid") ||
+          (v === 1 && env.grid === "bitmap")
+        ) {
+          matches = false;
+          break;
+        }
+      } else if (k === "color-gamut") {
+        const [belowSrgb, srgbAndBelowP3, p3AndBelowRec2020, rec2020AndAbove] =
+          p[k];
+        if (
+          (env.colorGamut === "notSrgb" && !belowSrgb) ||
+          (env.colorGamut === "srgbButNotP3" && !srgbAndBelowP3) ||
+          (env.colorGamut === "p3ButNotRec2020" && !p3AndBelowRec2020) ||
+          (env.colorGamut === "rec2020" && !rec2020AndAbove)
+        ) {
+          matches = false;
+          break;
+        }
+      } else if (k === "orientation") {
+        const v = p[k];
+        if (
+          (v === "portrait" && env.widthPx > env.heightPx) ||
+          (v === "landscape" && env.widthPx < env.heightPx)
+        ) {
+          matches = false;
+          break;
+        }
+      } else if (k === "overflow-block") {
+        const v = p[k];
+        if (v !== env.overflowBlock) {
+          matches = false;
+          break;
+        }
+      } else if (k === "overflow-inline") {
+        const v = p[k];
+        if (v !== env.overflowInline) {
+          matches = false;
+          break;
+        }
+      } else if (k === "scan") {
+        const v = p[k];
+        if (v !== env.scan) {
+          matches = false;
+          break;
+        }
+      } else if (k === "update") {
+        const v = p[k];
+        if (v !== env.update) {
+          matches = false;
+          break;
+        }
       } else if (k === "width") {
         const [minInclusive, min, max, maxInclusive] = p[k];
+        console.log(minInclusive, min, max, maxInclusive, env.widthPx);
+        console.log(
+          !(
+            env.widthPx < min ||
+            env.widthPx > max ||
+            (min === env.widthPx && !minInclusive) ||
+            (max === env.widthPx && !maxInclusive)
+          )
+        );
         if (
           env.widthPx < min ||
           env.widthPx > max ||
           (min === env.widthPx && !minInclusive) ||
           (max === env.widthPx && !maxInclusive)
+        ) {
+          matches = false;
+          break;
+        }
+      } else if (k === "device-width") {
+        const [minInclusive, min, max, maxInclusive] = p[k];
+        if (
+          env.deviceWidthPx < min ||
+          env.deviceWidthPx > max ||
+          (min === env.deviceWidthPx && !minInclusive) ||
+          (max === env.deviceWidthPx && !maxInclusive)
+        ) {
+          matches = false;
+          break;
+        }
+      } else if (k === "height") {
+        const [minInclusive, min, max, maxInclusive] = p[k];
+        if (
+          env.heightPx < min ||
+          env.heightPx > max ||
+          (min === env.heightPx && !minInclusive) ||
+          (max === env.heightPx && !maxInclusive)
+        ) {
+          matches = false;
+          break;
+        }
+      } else if (k === "device-height") {
+        const [minInclusive, min, max, maxInclusive] = p[k];
+        if (
+          env.deviceHeightPx < min ||
+          env.deviceHeightPx > max ||
+          (min === env.deviceHeightPx && !minInclusive) ||
+          (max === env.deviceHeightPx && !maxInclusive)
+        ) {
+          matches = false;
+          break;
+        }
+      } else if (k === "color") {
+        const [minInclusive, min, max, maxInclusive] = p[k];
+        if (
+          env.colorBits < min ||
+          env.colorBits > max ||
+          (min === env.colorBits && !minInclusive) ||
+          (max === env.colorBits && !maxInclusive)
+        ) {
+          matches = false;
+          break;
+        }
+      } else if (k === "monochrome") {
+        const [minInclusive, min, max, maxInclusive] = p[k];
+        if (env.monochromeBits === "not-monochrome") {
+          if (
+            min > 0 ||
+            (min === 0 && !minInclusive) ||
+            (max === 0 && !maxInclusive)
+          ) {
+            matches = false;
+            break;
+          }
+        }
+        if (
+          env.monochromeBits < min ||
+          env.monochromeBits > max ||
+          (min === env.monochromeBits && !minInclusive) ||
+          (max === env.monochromeBits && !maxInclusive)
+        ) {
+          matches = false;
+          break;
+        }
+      } else if (k === "resolution") {
+        const [minInclusive, min, max, maxInclusive] = p[k];
+        if (
+          env.dppx < min ||
+          env.dppx > max ||
+          (min === env.dppx && !minInclusive) ||
+          (max === env.dppx && !maxInclusive)
+        ) {
+          matches = false;
+          break;
+        }
+      } else if (k === "color-index") {
+        const [minInclusive, min, max, maxInclusive] = p[k];
+        if (env.colorTable === "none") {
+          if (
+            min > 0 ||
+            (min === 0 && !minInclusive) ||
+            (max === 0 && !maxInclusive)
+          ) {
+            matches = false;
+            break;
+          }
+        }
+        if (
+          env.colorTable < min ||
+          env.colorTable > max ||
+          (min === env.colorTable && !minInclusive) ||
+          (max === env.colorTable && !maxInclusive)
+        ) {
+          matches = false;
+          break;
+        }
+      } else if (k === "aspect-ratio") {
+        const [minInclusive, minRatio, maxRatio, maxInclusive] = p[k];
+        const min = minRatio[0] / minRatio[1];
+        const max = maxRatio[0] / maxRatio[1];
+        const aspectRatio = env.widthPx / env.heightPx;
+        if (
+          aspectRatio < min ||
+          aspectRatio > max ||
+          (min === aspectRatio && !minInclusive) ||
+          (max === aspectRatio && !maxInclusive)
+        ) {
+          matches = false;
+          break;
+        }
+      } else {
+        const [minInclusive, minRatio, maxRatio, maxInclusive] = p[k];
+        const min = minRatio[0] / minRatio[1];
+        const max = maxRatio[0] / maxRatio[1];
+        const aspectRatio = env.deviceWidthPx / env.deviceHeightPx;
+        if (
+          aspectRatio < min ||
+          aspectRatio > max ||
+          (min === aspectRatio && !minInclusive) ||
+          (max === aspectRatio && !maxInclusive)
         ) {
           matches = false;
           break;
