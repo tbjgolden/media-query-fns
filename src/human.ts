354647773dd7fd70599dc2352b89e260db649674
Tom <8162045+tbjgolden@users.noreply.github.com>
2022-03-22 11:49:22 +0000

    feat!: working ci npm releases

--- src/human.ts
+++ src/human.ts
@@ -4,7 +4,7 @@ export type HumanFriendlyData = any;
 export const toHumanFriendlyData = (
   result: EvaluateResult
 ): HumanFriendlyData => {
-  throw new Error("This function is not implemented");
+  throw new Error("This function is not yet implemented");
   console.log(result);
   //
   return [];
@@ -12,18 +12,18 @@ export const toHumanFriendlyData = (
 
 // assume a single line of text
 export const toHumanFriendlyString = (result: EvaluateResult): string => {
-  throw new Error("This function is not implemented");
+  throw new Error("This function is not yet implemented");
   return ``;
 };
 
 // divs and spans with inline styles, plus semantic where appropriate
 export const toHumanFriendlyHTML = (result: EvaluateResult): string => {
-  throw new Error("This function is not implemented");
+  throw new Error("This function is not yet implemented");
   return ``;
 };
 
 // use colors to syntax highlight
 export const toHumanFriendlyCLIString = (result: EvaluateResult): string => {
-  throw new Error("This function is not implemented");
+  throw new Error("This function is not yet implemented");
   return ``;
 };
